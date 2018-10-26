0468f8d2f438fa6d14cdacc259213f5ba7086aba
Milos Djordjevic <milos@themelaboratory.com>
2018-10-26 20:21:33 +0200

    added conditional

--- assets/plugins/neoresize/configs/nb.config.php
+++ assets/plugins/neoresize/configs/nb.config.php
@@ -5,6 +5,6 @@ $dr_local["tpl"]["all"] = <<<HTML
 <span class="caption [+dr.source.class+]" style="width:[+dr.source.width+]px;">
 <a href="[+dr.source.src+]" class="lightbox" data-fancybox-group="lightbox_group">
 <img src="[[phpthumb? &input=`[+dr.source.src+]` &options=`w=[+dr.source.width+],h=[+dr.source.height+],fltr=usm|40|1|3,zc=1,q-90`]]" width="[+dr.source.width+]" height="[+dr.source.height+]" alt="[+dr.source.alt+]" title="Click to enlarge" />
-<span class="description">[+dr.source.alt+]<span class="zoom-link">Zoom</span></span></a></span>
+<span class="description">[[if? &is=`[+dr.source.alt+]:notempty` &then=`[+dr.source.alt+]`]]<span class="zoom-link">Zoom</span></span></a></span>
 HTML;
 ?>
