bc73a194d8e4ab62aae70fa9af8626e2f0a741d2
Milos Djordjevic <milos@themelaboratory.com>
2018-11-13 15:01:44 +0100

    .

--- assets/plugins/neoresize/configs/nb.config.php
+++ assets/plugins/neoresize/configs/nb.config.php
@@ -4,7 +4,7 @@ $dr_local["tpl"]["all"] = <<<HTML
 
 <span class="caption [+dr.source.class+]" style="width:[+dr.source.width+]px;">
 <a href="[+dr.source.src+]" class="lightbox" data-fancybox="images" data-caption="[[if? &is=`[+dr.source.alt+]:notempty` &then=`[+dr.source.alt+]`]]">
-<img src="[[phpthumb? &input=`[+dr.source.src+]` &options=`w=[+dr.source.width+],h=[+dr.source.height+],fltr=usm|40|1|3,zc=1,q-90`]]" width="[+dr.source.width+]" height="[+dr.source.height+]" alt="[+dr.source.alt+]" title="Click to enlarge" />
+<img src="[[phpthumb? &cacheFolder=`assets/cache/images` &input=`[+dr.source.src+]` &options=`w=[+dr.source.width+],h=[+dr.source.height+],fltr=usm|40|1|3,zc=1,q-90`]]" width="[+dr.source.width+]" height="[+dr.source.height+]" alt="[+dr.source.alt+]" title="Click to enlarge" />
 <span class="description">[[if? &is=`[+dr.source.alt+]:notempty` &then=`[+dr.source.alt+]`]]<span class="zoom-link">Zoom</span></span></a></span>
 HTML;
 ?>
